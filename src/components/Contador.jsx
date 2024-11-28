10bfc070f0b4698596aa7930f918e20cb9a003b0
Lavken <lavkenpilar@gmail.com>
2024-11-28 16:42:42 -0300

    define-estado-cuenta

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Contador = () => {
+    const [cuenta, setCuenta] = useState (0)
   return (     
     <button onClick={()=>console.log("hola")}>Cuenta:0</button>
   )
