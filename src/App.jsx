a455026981777a3771eb2c7c5b74059bc1efedd1
Lavken <lavkenpilar@gmail.com>
2024-11-28 16:38:11 -0300

    createVite-contador.jsx--app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Contador from './components/Contador'
+
+function App() {
+  return (
+    <>
+     <h1>Contador</h1>
+     <Contador/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Contador.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contador = () => {
+  return (     
+    <button onClick={()=>console.log("hola")}>Cuenta:0</button>
+  )
+}
+
+export default Contador
